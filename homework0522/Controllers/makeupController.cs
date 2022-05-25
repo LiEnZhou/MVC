0e7cdf79e4f4052fe7a05170566f668b57338978
Lien Zhou <Lien.Zhou@shinda.com.tw>
2022-05-25 22:51:31 +0800

    Create功能

---
+++ homework0522/Controllers/makeupController.cs
@@ -0,0 +1,90 @@
+﻿using homework0522.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace homework0522.Controllers
+{
+    public class makeupController : Controller
+    {
+        private static List<Makeup> _makeups = new List<Makeup>()
+        {
+            new Makeup(){Id = 1, Name = "唇膏", price = 500, color = "red"},
+            new Makeup(){Id = 2, Name = "眼線", price = 450, color = "black"}
+        };
+        // GET: makeup
+        public ActionResult Index()
+        {
+            return View(_makeups);
+        }
+
+        // GET: makeup/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: makeup/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: makeup/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Makeup Model)
+        {
+            try
+            {
+              _makeups.Add(Model);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: makeup/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: makeup/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: makeup/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: makeup/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ homework0522/Models/Makeup.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace homework0522.Models
+{
+    public class Makeup
+    {
+
+        // 驗證屬性
+        // Validation Attribute
+        [Required]
+        [Range(1, 1000)]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Range(0, 100)]
+        public int price { get; set; }
+        [Required]
+        [Range(1, 100)]
+        public string? color { get; set; }
+    }
+}
